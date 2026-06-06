34b2e4a0e9f32338e0b43659215c3df8f823ca00
Taehyun Kim <tae6241@naver.com>
2026-06-06 11:28:39 +0900

    feat: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/taehyun/youthpolicyplatform/YouthPolicyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.taehyun.youthpolicyplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthPolicyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthPolicyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taehyun/youthpolicyplatform/YouthPolicyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehyun.youthpolicyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthPolicyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
